ed2227b84efa2d3dc0b63aebc03ea8d237ecbc96
Rahul <rchadalawada@sg.palo-it.com>
2024-05-04 21:17:22 +1000

    Update the Wifi Prints UI.

--- lib/bluetooth_print.dart
+++ lib/bluetooth_print.dart
@@ -8,35 +8,10 @@ class BluetoothPrinter {
     return devices.map((device) => Map<String, String>.from(device)).toList();
   }
 
-  static Future<String> connect(String address, String data) async {
-    final String result =
-        await _channel.invokeMethod('printToBluetoothPrinter', {"address": address, "data": data});
-    return result;
-  }
-
-  // static Future<String> pair(String address, String data) async {
-  //   final String result =
-  //       await _channel.invokeMethod('pair', {"address": address, "data": data});
-  //   return result;
-  // }
-
-  // static Future<String> print(String data) async {
-  //   final String result = await _channel.invokeMethod('print', {"data": data});
-  //   return result;
-  // }
-
   static Future<String?> printData(String address, String data) async {
     try {
-      // final String pairResult = await pair(address);
-      // if (pairResult == 'paired') {
-      final String result = await connect(address,data);
-      // if (result == 'connected') {
-      //   final String result = await print(data);
-      //   return result;
-      // }
+      final String result = await _channel.invokeMethod('printToBluetoothPrinter', {"address": address, "data": data});
       return result;
-      // }
-      // return pairResult;
     } catch (e) {
       return e.toString();
     }
--- lib/ui/printers/ip_device_list.dart
+++ lib/ui/printers/ip_device_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_printer_app/utils/ui_utils.dart';
+
 import 'bloc/ip_printer_bloc.dart';
 import 'bloc/ip_printer_event.dart';
 import 'bloc/ip_printer_state.dart';
@@ -14,10 +16,14 @@ class IPDeviceList extends StatefulWidget {
 }
 
 class _IPDeviceListState extends State<IPDeviceList> {
+  List<String> printers = [];
+  String loaderText = "Searching for nearby printers, this may take a while...";
+  TextEditingController ipController = TextEditingController();
+  bool searchingPrinters = false;
+
   @override
   void initState() {
     super.initState();
-    context.read<IPPrinterBloc>().add(FetchIPPrinters());
   }
 
   @override
@@ -26,50 +32,133 @@ class _IPDeviceListState extends State<IPDeviceList> {
       appBar: AppBar(
         title: const Text('Nearby IP Printers'),
       ),
-      body: BlocConsumer<IPPrinterBloc, IPPrinterState>(
-        listener: (context, state) {
-          if (state is IPPrinterPrinted) {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                title: const Text('Print Success'),
-                content: Text(state.result),
-                actions: <Widget>[
-                  TextButton(
-                    child: const Text('OK'),
-                    onPressed: () => Navigator.of(context).pop(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            if (!searchingPrinters)
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: ipController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter IP Address',
+                        labelText: 'IP Address',
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      String ipAddress = ipController.text.trim();
+                      if (ipAddress.isNotEmpty) {
+                        setState(() {
+                          loaderText = "Printing data...";
+                        });
+                        context.read<IPPrinterBloc>().add(
+                              ConnectAndPrint(
+                                deviceAddress: ipAddress,
+                                dataToPrint: widget.dataToPrint,
+                              ),
+                            );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Please enter IP address'),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Print'),
                   ),
                 ],
               ),
-            );
-          }
-        },
-        builder: (context, state) {
-          if (state is IPPrinterLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is IPPrintersLoaded) {
-            return ListView.builder(
-              itemCount: state.printers.length,
-              itemBuilder: (context, index) {
-                var printer = state.printers[index];
-                return ListTile(
-                  title: Text(printer),
-                  leading: const Icon(Icons.print),
-                  onTap: () => context.read<IPPrinterBloc>().add(
-                      ConnectAndPrint(
-                          deviceAddress: printer,
-                          dataToPrint: widget.dataToPrint
-                      )
+            if (!searchingPrinters)...[
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    searchingPrinters = true;
+                    loaderText = "Searching for nearby printers...";
+                  });
+                  context.read<IPPrinterBloc>().add(FetchIPPrinters());
+                },
+                child: Text('Search for Nearby Printers'),
+              ),
+            ],
+            if (searchingPrinters)
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircularProgressIndicator(),
+                      const SizedBox(height: 20),
+                      Text(loaderText),
+                    ],
+                  ),
+                ),
+              ),
+            if (!searchingPrinters)
+              Expanded(
+                child: BlocConsumer<IPPrinterBloc, IPPrinterState>(
+                  listener: (context, state) {
+                    if (state is IPPrinterPrinted) {
+                      UiUtils.showSuccessDialog(context, state.result);
+                    } else if (state is IPPrinterError) {
+                      UiUtils.showErrorDialog(context, state.message);
+                    } else if (state is IPPrintersLoaded) {
+                      setState(() {
+                        printers = state.printers;
+                      });
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is IPPrinterLoading) {
+                      return Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const CircularProgressIndicator(),
+                            const SizedBox(height: 20),
+                            Text(loaderText),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return _buildListView();
+                    }
+                  },
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListView() {
+    return ListView.builder(
+      itemCount: printers.length,
+      itemBuilder: (context, index) {
+        var printer = printers[index];
+        return ListTile(
+          title: Text(printer),
+          leading: const Icon(Icons.print),
+          onTap: () {
+            setState(() {
+              loaderText = "Printing data...";
+            });
+            context.read<IPPrinterBloc>().add(
+                  ConnectAndPrint(
+                    deviceAddress: printer,
+                    dataToPrint: widget.dataToPrint,
                   ),
                 );
-              },
-            );
-          } else if (state is IPPrinterError) {
-            return Center(child: Text(state.message));
-          }
-          return const Center(child: Text('Start searching for IP printers...'));
-        },
-      ),
+          },
+        );
+      },
     );
   }
 }
